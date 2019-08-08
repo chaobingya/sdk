795c1c69d9d4aed8f908c6f7e655622392953fa1
Dimitri Stiliadis <dstiliadis@gmail.com>
2019-08-07 22:31:29 -0700

    Basic example and readme file

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Aporeto SDK Documentation
+
+This repo contains a simple example for using the Aporeto client SDK.
+
+## Description
+
+The example application will create a namespace and the initialize an
+event subscriber and listen on events for the created namespace. It will
+listen for external networks and network access policy events only.
+The example demonstrates how the API client can be instantiated and how
+a subscriber can be defined that listens to specific events.
+
+## Related Libraries
+
+The example uses the Aporeto SKD that is mainly comprised of the following libraries:
+
+1. `manipulate`: is a generic client that can handle API CRUD operations for `elemental` objects. These objects model all the Aporeto API. (https://github.com/aporeto-inc/manipulate)
+
+2. `elemental`: is a generic library of models that represent API resources. (https://github.com/aporeto-inc/elemental)
+
+3. `gaia`: is the actual definition of the Aporeto API. (https://github.com/aporeto-inc/gaia)
+
+4. `midgard-lib`: is a library of helpers that deals with authentication against the platform. (https://github.com/aporeto-inc/midgard-lib)
+
+## Installation
+
+1. Create appcreds either in the Aporeto UI or using the apoctl utility. Store the appcreds file in the `./example` directory as sdk.json.
+
+2. Navigate to example and run it:
+
+```bash
+go run main.go
+```
+
+Always use a dependency manager to fix library versions to the current release of your installation.
---
+++ example/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"go.aporeto.io/elemental"
+	"go.aporeto.io/manipulate"
+	"go.uber.org/zap"
+
+	"go.aporeto.io/gaia"
+	"go.aporeto.io/manipulate/maniphttp"
+	midgardclient "go.aporeto.io/midgard-lib/client"
+	"go.aporeto.io/midgard-lib/tokenmanager"
+)
+
+const (
+	// The appcred must be created ahead of time.
+	credsPath = "./sdk.json"
+)
+
+func main() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	m, err := createAPIClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// Let's creata a new namespace under the current namespace.
+	ns := gaia.NewNamespace()
+	ns.Name = "test"
+
+	subctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	// Create a manipulate context. Note, that we use the subcontext.
+	// In case of failures, the manipulate client will do retries
+	// until the expiration of the context. If the error is not a
+	// timeout or communication error it will return immediately.
+	mctx := manipulate.NewContext(subctx)
+	if err := m.Create(mctx, ns); err != nil {
+		panic(fmt.Sprintf("unable to create namespace: %s", err))
+	}
+
+	// Note that the namespace name has been updated with the fully
+	// qualified path, even though we provided a relative namespace above.
+	fmt.Println("Succesfully created namespace:", ns.Name)
+
+	// Let's create a subscriber for the created namespace. It will
+	// listen to policy and external network events.
+	s := createSubscriber(ctx, m, ns.Name)
+
+	fmt.Printf("Listening for events in namespace: %s ... \n", ns.Name)
+	// We will start a loop now and listen to all events in the namespace
+	// and just print them.
+	for {
+		select {
+		case evt := <-s.Events():
+			eventHandler(evt)
+
+		case err := <-s.Errors():
+			zap.L().Error("Received error from the push channel", zap.Error(err))
+
+		case status := <-s.Status():
+
+			switch status {
+
+			case manipulate.SubscriberStatusDisconnection:
+				zap.L().Warn("Upstream event channel interrupted. Reconnecting...")
+
+			case manipulate.SubscriberStatusInitialConnection:
+				zap.L().Info("Upstream event channel connected")
+
+			case manipulate.SubscriberStatusReconnection:
+				zap.L().Info("Upstream event channel restored")
+
+			case manipulate.SubscriberStatusFinalDisconnection:
+				return
+			}
+
+		case <-ctx.Done():
+			return
+		}
+	}
+
+}
+
+// creteAPIClient creates a new API client. It uses appcreds
+// and configures auto-renewal of the tokens used by the manipulator.
+// Note that the API endpoint and root namespace are derived from
+// the appcreds automatically.
+func createAPIClient(ctx context.Context) (manipulate.Manipulator, error) {
+	// Read the appcreds from a local file. The appcreds
+	// have been created through apoctl, UI, or the API.
+	data, err := ioutil.ReadFile(credsPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read credential file: %s", err)
+	}
+
+	// Unmarshall the data in the data structure.
+	var appCred *gaia.Credential
+	appCred, tlsConfig, err := midgardclient.ParseCredentials(data)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse credential: %s", err)
+	}
+
+	return maniphttp.New(
+		ctx,
+		appCred.APIURL,
+		maniphttp.OptionNamespace(appCred.Namespace),
+		maniphttp.OptionTLSConfig(tlsConfig),
+		maniphttp.OptionTokenManager(
+			tokenmanager.NewX509TokenManager(
+				appCred.APIURL,
+				24*time.Hour,
+				tlsConfig,
+			),
+		),
+	)
+}
+
+// createSubscriber creates a new API subscriber. Note that the subscriber
+// is created listening on events in the namespace of the manipulator.
+func createSubscriber(ctx context.Context, m manipulate.Manipulator, namespace string) manipulate.Subscriber {
+	filter := elemental.NewPushFilter()
+	filter.FilterIdentity(gaia.NetworkAccessPolicyIdentity.Name)
+	filter.FilterIdentity(gaia.ExternalNetworkIdentity.Name)
+
+	s := maniphttp.NewSubscriber(
+		m,
+		maniphttp.SubscriberOptionRecursive(true),      // get events from children as well.
+		maniphttp.SubscriberOptionNamespace(namespace), //start at namespace
+	)
+	s.Start(ctx, filter)
+
+	return s
+}
+
+// eventHandler is a basic handler that just prints the events.
+func eventHandler(evt *elemental.Event) {
+
+	switch evt.Identity {
+
+	case gaia.ExternalNetworkIdentity.Name:
+
+		e := gaia.NewExternalNetwork()
+		if err := evt.Decode(&e); err != nil {
+			zap.L().Error("Failed to decode event", zap.Reflect("event", evt))
+		}
+
+		fmt.Printf("External network name: %s type %s\n", e.Name, evt.Type)
+
+	case gaia.NetworkAccessPolicyIdentity.Name:
+
+		p := gaia.NewNetworkAccessPolicy()
+		if err := evt.Decode(&p); err != nil {
+			zap.L().Error("Failed to decode event", zap.Reflect("event", evt))
+		}
+
+		fmt.Printf("Policy name: %s type %s\n", p.Name, evt.Type)
+
+	default:
+		zap.L().Error("Received event that was not subscribed", zap.Reflect("event", evt))
+	}
+}
